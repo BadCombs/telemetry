15c1cacc9ef5cb7923dde766abe4f33267976451
Giovanni <gpettinacci5@gmail.com>
2022-10-19 11:02:58 +0200

    Write message.h and message.cpp

---
+++ message.h
@@ -0,0 +1,25 @@
+#ifndef MESSAGE_H
+#define MESSAGE_H
+
+#include <cstdint>
+
+class message {
+	friend bool operator== (const message&, const message&);
+	
+	public:
+		message(char *, int);
+		message(const message&);
+		~message();
+		
+		// Getter
+		uint16_t get_id();
+		uint32_t get_payload();
+		bool is_correct();
+
+	private:
+		uint16_t id;
+		uint32_t payload;
+		bool correct;
+};
+
+#endif
